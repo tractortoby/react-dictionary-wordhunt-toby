3abeffb549bc03c7eaaee7154c06b90e311a8b5b
tractortoby <tobychung1993@gmail.com>
2022-01-10 21:05:35 +0800

    git add .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
+import Header from './components/Header/Header';
 
 function App() {
+  const [word, setWord] = useState('');
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState('en');
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`,
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className='App'
+      style={{
+        height: '100vh',
+        backgroundColor: '#282c32',
+        color: 'white',
+      }}
+    >
+      <Container
+        maxWidth='md'
+        style={{
+          display: 'flex',
+          flexDireation: 'column',
+          height: '100vh',
+        }}
+      >
+        <Header
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setWord={setWord}
+        />
+      </Container>
     </div>
   );
 }
